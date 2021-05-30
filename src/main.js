ea48ebf629a23997cb4bf8fb4f6289a3f494dad3
Mariia Kahlert <mariia.kahlert@gmail.com>
2021-05-30 15:01:09 +0200

    tests firestore

--- src/main.js
+++ src/main.js
@@ -3,5 +3,34 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import "./index.css";
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAmPg8_sVFmh_WR5wdOMLsIid5B_3N-nrk",
+  authDomain: "focus-app-spiced.firebaseapp.com",
+  projectId: "focus-app-spiced",
+  storageBucket: "focus-app-spiced.appspot.com",
+  messagingSenderId: "878915209438",
+  appId: "1:878915209438:web:c2cc16edcb75e17c085a4f",
+  measurementId: "G-G8LPWCXGHR",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// Firestore
+export const db = firebase.firestore();
 
 createApp(App).use(router).mount("#app");
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,4 +2,30 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <div>
+    <ul v-for="label in labels" :key="label.name">
+      <li>{{ label.name }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { db } from "../main";
+export default {
+  name: "About",
+  data: function () {
+    return { labels: [] };
+  },
+  props: {},
+  mounted: async function () {
+    try {
+      const labelsQuery = db.collection("labels").where("user_id", "==", "1");
+      labelsQuery.onSnapshot((snapshot) => {
+        this.labels = snapshot.docs.map((doc) => doc.data());
+      });
+    } catch (err) {
+      console.log("Error is About mounted: ", err);
+    }
+  },
+};
+</script>
