05cd684de8a368262b5b21b1824b2693a885db10
Mariia Kahlert <mariia.kahlert@gmail.com>
2021-05-31 15:18:42 +0200

    allows users to set their own timers

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,18 +1,24 @@
 <template>
   <button
-    class="
-      w-full
-      bg-yellow-800
-      text-yellow-50
-      px-8
-      py-4
-      rounded-full
-      shadow-lg
-      font-bold
-      focus:outline-none
-      focus:bg-yellow-700
-    "
+    :class="{
+      'bg-yellow-800 text-yellow-50 shadow-lg focus:bg-yellow-700':
+        design === 'filled',
+      'text-yellow-800 border border-yellow-800 focus:text-opacity-50 focus:border-opacity-50':
+        design === 'outlined',
+    }"
+    class="w-full px-8 py-4 rounded-full font-bold focus:outline-none"
   >
     <slot></slot>
   </button>
 </template>
+
+<script>
+export default {
+  props: {
+    design: {
+      type: String,
+      default: "filled",
+    },
+  },
+};
+</script>
--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,42 +1,67 @@
 <template>
-  <div class="h-full flex flex-col items-center justify-center">
-    <div
-      class="
-        border-8 border-yellow-800
-        rounded-full
-        flex
-        justify-center
-        items-center
-        bg-yellow-100
-        shadow-lg
-      "
-      style="height: 250px; width: 250px"
-    >
-      <div class="font-bold text-4xl text-yellow-800">
-        <input
-          type="number"
-          placeholder="00"
-          class="
-            bg-yellow-100
-            focus:outline-none
-            w-16
-            border-2 border-yellow-800
-            rounded-lg
-            px-2
-            mr-2
-            font-bold
-          "
-          min="0"
-        />
-        <span>:00</span>
-      </div>
+  <div class="h-full flex flex-col items-center justify-evenly">
+    <h1 v-if="!timerStarted && !congrats" class="text-center text-xl w-2/3">
+      Try setting your own timer up to 60 minutes and focus on your task
+    </h1>
+    <h1 v-else-if="!timerStarted && congrats" class="text-center text-xl w-2/3">
+      Congrats! You've managed to focus during {{ setTimeLimitInMin }}
+      {{ setTimeLimitInMin === "1" ? "minute" : "minutes" }}!
+    </h1>
+    <div class="flex flex-col items-center">
+      <div
+        class="
+          border-8 border-yellow-800
+          rounded-full
+          flex
+          justify-center
+          items-center
+          bg-yellow-100
+          shadow-lg
+          mt-12
+        "
+        style="height: 250px; width: 250px"
+      >
+        <div v-if="!timerStarted" class="font-bold text-4xl text-yellow-800">
+          <input
+            v-model="timeLimitInMin"
+            type="number"
+            placeholder="00"
+            class="
+              bg-yellow-100
+              focus:outline-none
+              w-16
+              border-2 border-yellow-800
+              rounded-lg
+              px-2
+              mr-2
+              font-bold
+            "
+            min="1"
+            max="60"
+          />
+          <span>:00</span>
+        </div>
 
-      <span v-if="false" class="font-bold text-4xl">
-        <!-- Remaining time label -->
-        {{ formattedTimeLeft }}
-      </span>
+        <span v-else class="font-bold text-4xl">
+          {{ formattedTimeLeft }}
+        </span>
+      </div>
+      <template v-if="!timerStarted">
+        <Button class="mt-12 text-xl" @click="startTimer">Focus</Button>
+      </template>
+
+      <Button
+        design="outlined"
+        class="mt-12 text-lg"
+        v-else-if="cancelTimeCountdown"
+        @click="onTimesUp"
+      >
+        Cancel ({{ cancelTimeCountdown }})
+      </Button>
     </div>
-    <Button class="mt-12 text-xl" @click.prevent="startTimer">Focus</Button>
+    <router-link v-if="!timerStarted" to="/welcome" class="text-lg"
+      >Back</router-link
+    >
   </div>
 </template>
 
@@ -44,29 +69,45 @@
 export default {
   data: function () {
     return {
-      timeLimit: 15,
+      setTimeLimitInMin: null,
+      timeLimitInMin: null,
       timePassed: 0,
       timerInterval: null,
+      timerStarted: false,
+      cancelTimeInSec: 10,
+      congrats: false,
     };
+  },
+  mounted: function () {
+    this.timerStarted;
   },
   watch: {
     timePassed(newValue) {
-      if (newValue === this.timeLimit) {
+      if (newValue === this.timeLimitInMin * 60) {
         this.onTimesUp();
       }
     },
   },
   methods: {
     startTimer: function () {
-      this.timerInterval = setInterval(() => (this.timePassed += 1), 1000);
+      if (this.timeLimitInMin) {
+        this.timerStarted = true;
+        this.setTimeLimitInMin = this.timeLimitInMin;
+        this.timerInterval = setInterval(() => (this.timePassed += 1), 1000);
+      }
     },
     onTimesUp: function () {
       clearInterval(this.timerInterval);
+      this.timerStarted = false;
+      if (this.timeLimitInMin * 60 === this.timePassed) {
+        this.congrats = true;
+      }
+      this.timeLimitInMin = null;
     },
   },
   computed: {
     calculateTimeLeft: function () {
-      return this.timeLimit - this.timePassed;
+      return this.timeLimitInMin * 60 - this.timePassed;
     },
     formattedTimeLeft: function () {
       const timeLeft = this.calculateTimeLeft;
@@ -76,6 +117,9 @@ export default {
         seconds = `0${seconds}`;
       }
       return `${minutes}:${seconds}`;
+    },
+    cancelTimeCountdown: function () {
+      return 10 - this.timePassed >= 0 ? 10 - this.timePassed : null;
     },
   },
 };
